98d631cd826557624a28ea4a8bd36de0d0f43e21
diegoperea20 <diegoperea20@gmail.com>
2024-06-05 14:46:21 -0500

    update link github

--- src/app/page.js
+++ src/app/page.js
@@ -83,7 +83,7 @@ export default function Home() {
       </div>
       <div className="project-github">
         <p>This project is in </p>
-        <Link href="https://github.com/diegoperea20"><img width="96" height="96" src="https://img.icons8.com/fluency/96/github.png" alt="github"/></Link>
+        <Link href="https://github.com/diegoperea20/Nextjs-classification-cats-and-dogs-with-tensorflowjs"><img width="96" height="96" src="https://img.icons8.com/fluency/96/github.png" alt="github"/></Link>
       
       </div>
 
